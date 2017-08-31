9bf3347b4f7f6b19501227e3a3e0dc84b191eaaa
lephongvu90 <lephongvu90@gmail.com>
2017-08-31 21:49:07 +0700

    new

---
+++ InterviewPratice/Arrays/firstNotRepeatingCharacter.cpp
@@ -0,0 +1,48 @@
+/* https://codefights.com/interview-practice/task/uX5iLwhc6L5ckSyNC
+
+Note: Write a solution that only iterates over the string once and uses O(1) additional memory, since this is what you would be asked to do during a real interview.
+
+Given a string s, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
+
+Example
+
+For s = "abacabad", the output should be
+firstNotRepeatingCharacter(s) = 'c'.
+
+There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since it appears in the string first.
+
+For s = "abacabaabacaba", the output should be
+firstNotRepeatingCharacter(s) = '_'.
+
+There are no characters in this string that do not repeat.
+
+Input/Output
+
+[time limit] 500ms (cpp)
+[input] string s
+
+A string that contains only lowercase English letters.
+
+Guaranteed constraints:
+1 ≤ s.length ≤ 105.
+
+[output] char
+
+The first non-repeating character in s, or '_' if there are no characters that do not repeat. */
+
+char firstNotRepeatingCharacter(std::string s) {
+    
+    int a[26] = {0};
+    
+    for(auto c:s)
+    {
+        a[c-'a']++;
+    }
+    for(auto c:s)
+    {
+        if(a[c-'a'] == 1)
+            return c;
+    }
+    return '_';
+}
+
